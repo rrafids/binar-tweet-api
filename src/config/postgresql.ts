e90cfb5a2d506d83c4175440ad1d25aa83f2151b
Raihan Rafid <raihanrfd1@gmail.com>
2023-12-22 10:41:19 +0700

    fix db env: add host

--- src/config/postgresql.ts
+++ src/config/postgresql.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const knexInstance = knex({
   client: process.env.DB_CLIENT,
   connection: {
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
