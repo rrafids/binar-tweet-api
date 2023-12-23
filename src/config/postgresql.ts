ebc1d8f0987432ed63f3d507ebe5cdbce430905b
Raihan Rafid <raihanrfd1@gmail.com>
2023-12-23 10:43:52 +0700

    add db port

--- src/config/postgresql.ts
+++ src/config/postgresql.ts
@@ -9,6 +9,7 @@ const knexInstance = knex({
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
   },
 });
 
