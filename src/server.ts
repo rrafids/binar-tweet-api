fb87f272428562f30cb940428d16bf4c9d4c95dd
Raihan Rafid <raihanrfd1@gmail.com>
2023-12-23 10:22:26 +0700

    rm default url

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const PORT = Number(process.env.APP_PORT) || 443;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.APP_PORT}`);
 });
