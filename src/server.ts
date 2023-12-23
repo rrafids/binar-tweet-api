761918168db208d9980022ef48ff569001565dd6
Raihan Rafid <raihanrfd1@gmail.com>
2023-12-23 10:35:56 +0700

    update port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const PORT = Number(process.env.PORT) || 443;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
